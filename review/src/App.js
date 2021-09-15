201b283e26e840f4d1d0493a1beef4ee04583460
LuiCoro <luiacorona029@icloud.com>
2021-09-14 19:44:14 -0700

    'First Code Break'
    - About to clean up code
    - the type has to match the case name
    - created function to help clean up the initial code

--- review/src/App.js
+++ review/src/App.js
@@ -1,13 +1,47 @@
-import React, { useReducer } from "react";
+import React, {useReducer} from "react";
 import reducer from './reducers/calcReducer';
 import "./styles.css";
 
+// take our action objects - {type: 'ADD', payload: 7}
+// make
+// action creators - functions that return action object
+const addAction = (input) => {
+  return ({type: 'ADD', payload: input})
+}
+
+const subAction = (input) => {
+  return ({type:'SUBTRACT', payload: input})
+}
+
+const memoryAction = (input) => {
+  return ({type: 'ADD_TO_MEMORY'})
+}
+
+const clearAction = (input) => {
+  return ({type: 'CLEAR'})
+}
+
+let currentState = {
+  currentValue: 0,
+  memory: 0
+}
+
+currentState = reducer(currentState, addAction(15))
+console.log(currentState)
+currentState = reducer(currentState,subAction(7))
+console.log(currentState)
+currentState = reducer(currentState, addAction(4))
+console.log(currentState)
+currentState = reducer(currentState, memoryAction())
+console.log(currentState)
+currentState = reducer(currentState, clearAction())
+console.log(currentState)
 
 export default function App() {
   return (
     <div className="App">
       <textarea rows="1" value="0" id="total" type="text" name="ans"></textarea>
-      <br />
+      <br/>
       <button type="button" className="btn">
         +
       </button>
